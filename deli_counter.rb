0e0e41a832a5df470ac36ecf472927ce152a31ff
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-23 19:18:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,9 +23,7 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.each do |customer|
-      puts "Currently serving #{customer}."
-      katz_deli.unshift()
+
     end
   end
 end

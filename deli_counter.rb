4c788cf7f32696920bc197d89bd2c9d4681fe10d
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-23 18:53:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
-def line(kats_deli)
-  if katz_deli.size == 0
+def line(deli)
+  number = 1
+  if number > 0
+  deli.each do |customer|
+    puts "The line is currently: #{number}. #{customer}."
+  else
     puts "The line is currently empty."
   end
+end
 end

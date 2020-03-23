c37b7f7e3f24a5a663051c2cc596fb973f2650cb
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-23 18:57:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,15 @@
-def line(deli)
-  number = 1
-  if number > 0
-  deli.each do |customer|
-    puts "The line is currently: #{number}. #{customer}."
-    number += 1
-  end
-elsif deli.size == 0
+katz_deli = []
+
+def line(katz_deli)
+  line_positions = [];
+  if katz_deli.empty?
     puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
